33d1fb3e07c1d8708fbafd0aa1056a2a12f6e9a6
praveenkumar2661 <praveenkumar1856399@gmail.com>
2024-03-09 00:15:20 +0530

    BetsyNumber

---
+++ VidyaDoubts/BetsyNumber.java
@@ -0,0 +1,63 @@
+package VidyaDoubts;
+public class BetsyNumber 
+{
+	public static void main(String[] args) 
+	{
+		int start = 100;
+		int end = 500;
+		int ans = 0;
+		for (int i = start; i < end; i++) 
+		{
+			if(betsyCheckF(i) || betsyCheckR(i))
+			{
+				ans+=i;
+				System.out.println(i);
+			}
+		}
+		System.out.println(ans);
+	}
+	static boolean betsyCheckF(int num)
+	{
+		if(num<=10)
+			return true;
+		else
+		{
+			String n = Integer.toString(num);
+			int count = 0;
+			int pivot = n.length()-1;
+			int i = 0;
+			while(pivot!=0)
+			{
+				if((n.charAt(i+1)-n.charAt(i) == 1))
+					count++;
+				i++;
+				pivot--;
+			}
+			if(count == n.length()-1)
+				return true;
+		}
+		return false;
+	}
+	static boolean betsyCheckR(int num)
+	{
+		if(num<=10)
+			return true;
+		else
+		{
+			String n = Integer.toString(num);
+			int count = 0;
+			int pivot = n.length()-1;
+			int i = 0;
+			while(pivot!=0)
+			{
+				if((n.charAt(i+1)-n.charAt(i) == -1))
+					count++;
+				i++;
+				pivot--;
+			}
+			if(count == n.length()-1)
+				return true;
+		}
+		return false;
+	}
+}
