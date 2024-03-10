efc5678e2a86188017096e333c6ff41f929bf381
praveenkumar2661 <praveenkumar1856399@gmail.com>
2024-03-10 21:44:51 +0530

    KunaL

---
+++ KunalKushwaha/FloorOfAnNumber.java
@@ -0,0 +1,29 @@
+package KunalKushwaha;
+
+public class FloorOfAnNumber 
+{
+	public static void main(String[] args) 
+	{
+		int[] arr = {2,3,5,9,14,16,18};
+		int target = 15;
+		System.out.println(searchIndex(arr, target));
+	}
+	
+	private static int searchIndex(int[] arr, int target) 
+	{
+		int start = 0;
+		int end = arr.length-1;
+		
+		while(start<=end)
+		{
+			int mid = (start+end)/2;
+			if(arr[mid] == target)
+				return mid;
+			else if(arr[mid]>target)
+				end = mid-1;
+			else
+				start = mid+1;	
+		}
+		return arr[end];
+	}
+}
