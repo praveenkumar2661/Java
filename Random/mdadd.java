4c47d8d434918a1bcb3484447faae7ed0c5031c8
praveenkumar2661 <praveenkumar1856399@gmail.com>
2024-04-30 23:29:34 +0530

    Random

--- Random/mdadd.java
+++ Random/mdadd.java
@@ -1,5 +1,7 @@
 package Random;
 
+import java.util.Arrays;
+
 public class mdadd 
 {
 	public static void main(String[] args) {
@@ -12,19 +14,24 @@ public class mdadd
 //			}
 //			System.out.println();
 //		}
-		int[] array = {9,9,9};
-		int fmax = array[0];
-		int smax = array[0];
-		for(int i = 0; i < array.length; i++)
-		{
-			if(array[i]>fmax)
-			{
-				smax = fmax;
-				fmax = array[i];
-			}
-			else if(array[i]>smax && smax != fmax )
-				smax = array[i];
-		}
-		System.out.println(smax);
+//		int[] array = {9,9,9};
+//		int fmax = array[0];
+//		int smax = array[0];
+//		for(int i = 0; i < array.length; i++)
+//		{
+//			if(array[i]>fmax)
+//			{
+//				smax = fmax;
+//				fmax = array[i];
+//			}
+//			else if(array[i]>smax && smax != fmax )
+//				smax = array[i];
+//		}
+//		System.out.println(smax);
+		
+		
+		String s = "<html><head><title>Page Title</title></head><body><h1>This is a Heading</h1><p>This is a paragraph.</p></body></html>";
+		String[] arr = s.split("<");
+		System.out.println(Arrays.toString(arr));
 	}
 }
